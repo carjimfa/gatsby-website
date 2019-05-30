c7e198fd390607d67454ad1e91a83a6b3a13f3ab
carjimfa <carjimfa@gmail.com>
2019-05-30 14:07:37 +0200

    Fixes intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,9 +5,9 @@ import fitty from "fitty";
 export default () => (
   <div className="intro">
     <div className="title">
-        <h1 style={{'font-size': '3.26vw'}}>Carlos</h1>
-        <h1 style={{'font-size': '2.79vw'}}>Jiménez</h1>
-        <h5 style={{'font-size': '1.151vw'}}>Software Engineer</h5>
+        <h1 style={{'font-size': '3vw'}}>Carlos</h1>
+        <h1 style={{'font-size': '2.58vw'}}>Jiménez</h1>
+        <h5 style={{'font-size': '1.06vw'}}>Software Engineer</h5>
     </div>     
     <div className="msga">
       <h2><span>Make Software</span> Great <span>Again</span></h2>  
