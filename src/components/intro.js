fdfc1486b99a83dfe2a687f4c3a85bcfb22f0260
carjimfa <carjimfa@gmail.com>
2019-07-26 12:29:33 +0200

    Fixes bugs in intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -8,12 +8,12 @@ import "animate.min.css";
 export default () => (
   <div className="intro" id="intro">
     <div className="title">
-        <h1 className="glitch cray" data-text="CARLOS" style={{'font-size': '3vw'}}>Carlos</h1>
-        <h1 className="glitch cray" data-text="JIMENEZ" style={{'font-size': '2.58vw'}}>Jiménez</h1>
-        <h5 className="glitch cray" data-text="Software Engineer" style={{'font-size': '1.06vw'}}>Software Engineer</h5>
+        <h1 style={{'font-size': '3vw'}}>Carlos</h1>
+        <h1 style={{'font-size': '2.58vw'}}>Jiménez</h1>
+        <h5 style={{'font-size': '1.06vw'}}>Software Engineer</h5>
     </div>     
     <div className="msga glitch">
-      <h2 className="glitch cray" data-text="Make Software Great Again"><span>Make Software</span> Great <span>Again</span></h2>  
+      <h2>Make Software <span className="glitch cray" data-text="Great">Great</span> Again</h2>  
     </div>
   </div>
   
